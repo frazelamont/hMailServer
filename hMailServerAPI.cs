1b8657fc1f3199010ce1594c749d8189d59a9d64
Fxstar.eu <hello@fxstar.eu>
2017-04-22 11:05:50 +0200

    Update hMailServerAPI.cs

--- hMailServerAPI.cs
+++ hMailServerAPI.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using hMailServer;
 
+//Add MySql Library
+using MySql.Data.MySqlClient;
+
+
 // https://www.codeproject.com/Articles/1101963/Tips-on-hMailServer-Manipulation-using-Csharp
 // Add reference to hMailserver Server COM Library
 
@@ -12,6 +16,36 @@ namespace hMailAddAccount
 {
     class Program
     {
+        
+
+        private MySqlConnection Conn(){                                
+            string connectionString = "SERVER=localhost;DATABASE=hmail;UID=rooot;PASSWORD=toor;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+
+
+        // INSERT INTO `hm_domains` (`domainid`, `domainname`, `domainactive`, `domainpostmaster`, `domainmaxsize`, `domainaddomain`, `domainmaxmessagesize`, `domainuseplusaddressing`, `domainplusaddressingchar`, `domainantispamoptions`, `domainenablesignature`, `domainsignaturemethod`, `domainsignatureplaintext`, `domainsignaturehtml`, `domainaddsignaturestoreplies`, `domainaddsignaturestolocalemail`, `domainmaxnoofaccounts`, `domainmaxnoofaliases`, `domainmaxnoofdistributionlists`, `domainlimitationsenabled`, `domainmaxaccountsize`, `domaindkimselector`, `domaindkimprivatekeyfile`) VALUES (NULL, 'breakermind.com', '1', '', '0', '', '0', '0', '', '0', '0', '1', '', '', '0', '0', '0', '0', '0', '0', '0', '', '');
+        // INSERT INTO `hm_accounts` (`accountid`, `accountdomainid`, `accountadminlevel`, `accountaddress`, `accountpassword`, `accountactive`, `accountisad`, `accountaddomain`, `accountadusername`, `accountmaxsize`, `accountvacationmessageon`, `accountvacationmessage`, `accountvacationsubject`, `accountpwencryption`, `accountforwardenabled`, `accountforwardaddress`, `accountforwardkeeporiginal`, `accountenablesignature`, `accountsignatureplaintext`, `accountsignaturehtml`, `accountlastlogontime`, `accountvacationexpires`, `accountvacationexpiredate`, `accountpersonfirstname`, `accountpersonlastname`) VALUES (NULL, '8', '0', 'info@breakermind.com', '09598b56eddb0b17ca5e9585d70e989f343dc58706fdfd95fceba397f18dd100f084b5', '1', '0', '', '', '0', '0', '', '', '3', '0', '', '0', '0', '', '', '2017-04-06 19:45:44', '0', '2017-04-03 00:00:00', '', '');
+
+        private void GetAccounts(MySqlConnection conn){
+            conn.Open();
+            string query = "SELECT * FROM NewsletterAccounts";
+            
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                string account = dataReader["id"].ToString() + dataReader["name"] + dataReader["age"];
+            }
+
+            //cmd.ExecuteNonQuery();
+            dataReader.Close();
+            conn.Close();
+        }
+
+
         // Authenticate
         private static hMailServer.Application Authenticate(string userName,string password)
         {
@@ -26,6 +60,7 @@ namespace hMailAddAccount
             hMailServer.Application hMailApp = Authenticate(user, pass);
             hMailServer.Domain domain = hMailApp.Domains.Add();
             domain.Name = domainName;
+            domain.Postmaster = "catchall@" + domainName; // add catch all address, mailbox
             domain.Active = true;
             domain.Save();
             return true;
@@ -37,6 +72,17 @@ namespace hMailAddAccount
             hMailServer.Application hMailApp = Authenticate(userName, password);
             hMailServer.Domain myDomain = hMailApp.Domains.ItemByName[domainName];
             myDomain.Active = active;
+            myDomain.Save();
+            return true;
+        }
+
+        // Domain set catchall mailbox
+        private static bool DomainActivateCatchAll(string userName, string password, string domainName, string mailboxEmail = "catchall", bool active = true)
+        {
+            hMailServer.Application hMailApp = Authenticate(userName, password);
+            hMailServer.Domain myDomain = hMailApp.Domains.ItemByName[domainName];
+            myDomain.Active = active;
+            myDomain.Postmaster = mailboxEmail + "@" + domainName;
             myDomain.Save();
             return true;
         }
@@ -142,7 +188,7 @@ namespace hMailAddAccount
         static void Main(string[] args)
         {
             // Create new domain
-            DomainCreate("Administrator", "Password", "zenek.xx");
+            DomainCreate("Administrator", "xxxx", "xxxxx.xx");
         }
     }
 }
