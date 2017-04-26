e5b0d12decc287da8728fa1da8e6a79e9c75d0c2
Fxstar.eu <hello@fxstar.eu>
2017-04-26 21:29:18 +0200

    Com Api php
    Com Api php

---
+++ ComApi.php
@@ -0,0 +1,27 @@
+<?php
+// Connect to hMailServer !!!!!
+// See PHPWebAdmin folder in hmail server installation !!!!!
+try
+{
+	$domainid = 14;
+	$accountid = 54;
+	$obBaseApp = new COM("hMailServer.Application", NULL, CP_UTF8);
+	$obBaseApp->Connect();
+	// Authenticate the user
+   	$obBaseApp->Authenticate('Administrator', 'Password');
+   	// Get domain with id
+	$obDomain = $obBaseApp->Domains->ItemByDBID($domainid);
+	
+	// Get account with id
+	$obAccount = $obDomain->Accounts->ItemByDBID($accountid);
+	//$obAccount->Save();
+	// $accountid = $obAccount->ID;
+}
+catch(Exception $e)
+{
+   echo $e->getMessage();
+   echo "<br>";
+   echo "This problem is often caused by DCOM permissions not being set.";
+   die;
+}
+?>
